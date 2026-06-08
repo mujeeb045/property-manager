614e7286cde3f2d17764ba72637c8709cfbc2e65
Mujeeb <mujeeb045@gmail.com>
2026-06-08 21:10:02 +0530

    Local PostgreSQL migration successful

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL
 });
 
 async function initDatabase() {
